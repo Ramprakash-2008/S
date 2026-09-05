5dd61d4823719e62a2fa53003f25f48eb4beed4c
Ramprakash-2008 <rramp762008@gmail.com>
2026-09-05 10:58:51 +0530

    Update settings.py

--- securecrypt/settings.py
+++ securecrypt/settings.py
@@ -140,7 +140,8 @@ TEMPLATES = [
 # ============================================================
 # DATABASE
 # ============================================================
-
+print("DEBUG =", DEBUG)
+print("DB_HOST =", os.environ.get("DB_HOST"))
 if os.environ.get("DB_HOST"):
 
     # Production PostgreSQL
