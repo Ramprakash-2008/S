8f2417fe8d469e25a74e6bbe8f671ab19a2a427d
Ramprakash-2008 <rramp762008@gmail.com>
2026-09-06 11:46:46 +0530

    Update settings.py

--- securecrypt/settings.py
+++ securecrypt/settings.py
@@ -212,19 +212,9 @@ USE_TZ = True
 # EMAIL CONFIGURATION
 # ============================================================
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-EMAIL_HOST = "smtp.gmail.com"
-
-EMAIL_PORT = 587
-
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+BREVO_API_KEY = os.environ.get("BREVO_API_KEY")
+BREVO_SENDER_EMAIL = os.environ.get("BREVO_SENDER_EMAIL")
+BREVO_SENDER_NAME = os.environ.get("BREVO_SENDER_NAME", "SecureCrypt")
 
 
 # ============================================================
