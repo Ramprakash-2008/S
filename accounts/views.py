9ea6a78406d956f4589f60475b0e0c0855891e33
Ramprakash-2008 <rramp762008@gmail.com>
2026-09-07 21:35:13 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2650,7 +2650,7 @@ def encrypt_file_view(request):
                email_sent = send_encrypted_file(
                 recipient_email,
                 encrypted_file.encrypted_file.path,
-                encrypted_name + ".dat"
+                encrypted_name + ".zip"
                 )
 
             except Exception as email_error:
@@ -2737,11 +2737,11 @@ def decrypt_file_view(request):
 
         if not (
     filename.endswith(".enc") or
-    filename.endswith(".enc.dat")
+    filename.endswith(".enc.zip")
         ):
             messages.error(
         request,
-        "Please upload a valid .enc or .enc.dat encrypted file."
+        "Please upload a valid .enc or .enc.zip encrypted file."
     )
 
             return render(
